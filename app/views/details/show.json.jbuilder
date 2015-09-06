44f5d12696db1c81996c72a7cc7cb916ee460380
DAVID WANG <davidw294@gmail.com>
2015-09-06 01:38:39 -0400

    deleting files such as detail and scaffolds

--- app/views/details/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @detail, :id, :title, :created_at, :updated_at
--- db/migrate/20150906021518_create_details.rb
+++
@@ -1,9 +0,0 @@
-class CreateDetails < ActiveRecord::Migration
-  def change
-    create_table :details do |t|
-      t.string :title
-
-      t.timestamps null: false
-    end
-  end
-end
